29a9b7bdb3e9e6b1aa2977af93b906ad4b5744ba
Kasper Sandin <kasper.sandin@gmail.com>
2021-07-10 14:13:51 +0200

    Track viewport size

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,6 +1,7 @@
 import { useMouse } from "react-use";
 import { useRef } from "react";
 import { styled } from "@stitches/react";
+import useResizeObserver from "use-resize-observer";
 import { globalStyle } from "../globalStyle";
 import { createTransformStyle } from "../functions/createTransformStyle";
 import { useSelector } from "../state/store";
@@ -11,6 +12,7 @@ export function App() {
   const oceanRef = useRef<HTMLDivElement>(null);
   const ships = useSelector((state) => state.ships);
   const mouse = useMouse(oceanRef);
+  const { width, height } = useResizeObserver({ ref: oceanRef });
   globalStyle();
   return (
     <Ocean ref={oceanRef}>
@@ -18,15 +20,19 @@ export function App() {
         const ship = ships.entities[id]!;
         return <Ship key={id} style={createTransformStyle(ship.transform)} />;
       })}
-      <Info>{JSON.stringify(mouse)}</Info>
+      <Info>
+        <pre>
+          {JSON.stringify({ mouse, viewport: { width, height } }, null, 2)}
+        </pre>
+      </Info>
     </Ocean>
   );
 }
 
 const Info = styled("div", {
   position: "absolute",
-  top: 0,
-  left: 0,
+  top: 12,
+  left: 12,
   background: "rgba(0, 0, 0, 0.5)",
   color: "white",
   padding: 12,
